494bf27259df587de077a761fc6ad2d398b5d5a6
GAMAL ALI <gamalali@GAMALs-MacBook-Pro.local>
2018-11-15 16:50:29 -0500

    Docker configuration for Flask

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,8 +3,11 @@ from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
-# import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 # Pull in the config 
+# Retrieve the environment variable with os.getenv // returns None if one does not exist
+app_settings = os.getenv('APP_SETTINGS')
+print(app_settings)
 app.config.from_object('project.config.DevelopmentConfig')
 
 @app.route('/flowers/flower', methods=['GET'])
