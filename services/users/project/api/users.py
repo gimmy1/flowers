3447df9769f52d58c3372c82e530f1446862067e
GAMAL ALI <gamalali@GAMALs-MacBook-Pro.local>
2018-11-21 16:07:13 -0500

    TDD restful routes

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from project.api.models import User
+
+from project import db # import SQLAlchemy
+
+from sqlalchemy import exc
 
 # create a new instance of Blueprint
 flowers_blueprint = Blueprint('flowers', __name__)
@@ -9,3 +14,68 @@ def flower():
     return jsonify({
         "flower": "soon"
     })
+
+@flowers_blueprint.route('/flowers', methods=['GET'])
+def get_all_users():
+    """ Add flower user functionality && Retrieve all Users """
+    response_object = {
+        'message': 'success',
+        'data': {
+            'users': [user.to_json() for user in User.query.all()]
+        }
+    }
+    return jsonify(response_object), 200
+
+@flowers_blueprint.route('/flowers', methods=['POST', 'GET'])
+def flower_user():
+    post_data = request.get_json()
+    response_object = {
+        'message': 'Invalid payload'
+    }
+
+    # check for post_data
+    if not post_data:
+        return jsonify(response_object), 400
+    
+    username = post_data.get('username')
+    email = post_data.get('email')
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_object['message'] = f'{email} was added'
+            return jsonify(response_object), 201
+        else:
+            response_object['message'] = 'Sorry. That email already exists'
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        return jsonify(response_object), 400
+    
+
+@flowers_blueprint.route('/flowers/<user_id>', methods=['GET'])
+def get_single_user(user_id):
+    response_object = {
+        'message': 'User does not exist'
+    }
+    try:
+        user = User.query.filter_by(id=int(user_id)).first()
+        if not user:
+            return jsonify(response_object), 404
+        else:    
+            response_object = {
+                'message': f'{user.email} exists',
+                'data': {
+                    'id': user.id,
+                    'username': user.username, 
+                    'email': user.email
+                }
+            }
+            return jsonify(response_object), 200
+    except ValueError as ve:
+        return jsonify(response_object), 404
+    
+
+
+
--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -5,7 +5,14 @@ import json
 import unittest
 
 from project.tests.base import BaseTestCase
+from project.api.models import User
+from project import db
 
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
 
 class TestUserService(BaseTestCase):
     """Tests for the Users Service."""
@@ -16,6 +23,108 @@ class TestUserService(BaseTestCase):
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
         self.assertIn('soon', data['flower'])
+    
+    """ Test Add User Functionality """
+    def test_add_users(self):
+        with self.client:
+            response = self.client.post(
+                '/flowers',
+                data = json.dumps({
+                    'username': 'gamal',
+                    'email': 'gamal@gamal.com'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('gamal@gamal.com was added', data['message'])
+        
+
+    def test_add_users_invalid_json(self):
+        """ Error is thrown if empty json """
+        with self.client:
+            response = self.client.post(
+                '/flowers',
+                data = json.dumps({}),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload', data['message'])
+        
+    def test_add_users_invalid_json_keys(self):
+        """ Error is thrown if invalid json keys """
+        with self.client:
+            response = self.client.post(
+                '/flowers',
+                data=json.dumps({
+                    'email': 'gamal@gamal.com'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload', data['message'])
+        
+    def test_add_users_duplicate_email(self):
+        with self.client:
+            self.client.post(
+                '/flowers',
+                data = json.dumps({
+                    'username': 'gamal',
+                    'email': 'gamal@gamal.com'
+                }),
+                content_type='application/json'
+            )
+            response = self.client.post(
+                '/flowers',
+                data = json.dumps({
+                    'username': 'gamal',
+                    'email': 'gamal@gamal.com'
+                }),
+                content_type='application/json'
+            )
+
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Sorry. That email already exists', data['message'])
+        
+    """ Test Single User Functionality """
+    def test_get_single_user(self):
+        """ Ensure single user behaves correctly """
+        user = add_user('gamal', 'gamal@gamal.com')
+        import pdb; pdb.set_trace()
+        with self.client:
+            response = self.client.get(f'/flowers/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('gamal@gamal.com exists', data['message'])
+        
+    def test_single_user_no_id(self):
+        """ Error is thrown if id not provided """
+        response = self.client.get(f'/flowers/blah')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 404)
+        self.assertIn('User does not exist', data['message'])
+    
+    def test_single_user_incorrect_id(self):
+        """ Ensure error is thrown if incorrect id is provided """
+        response = self.client.get('flowers/888')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 404)
+        self.assertIn('User does not exist', data['message'])
+
+    """ Test Get All Users Functionality """
+    def test_all_users(self):
+        user1 = add_user('laurie', 'laurie@laurie.com')
+        user2 = add_user('gamal', 'gamal@gamal.com')
+
+        with self.client:
+            response = self.client.get('/flowers')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 2)
+            self.assertIn(data['message'], 'success')
 
 
 if __name__ == '__main__':
